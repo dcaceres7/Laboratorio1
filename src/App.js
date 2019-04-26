c76d6ff67fee1c9b48ed91faa05eeff05ee4ba88
dcaceres7 <dancacers_95@unitec.edu>
2019-04-25 23:59:17 -0600

    Laboratorio 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import UnitecClass from './Components/UnitecClass';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <UnitecClass unitecStudentName= "Daniela Caceres"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/UnitecClass.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+
+class UnitecClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { unitecClassName: 'CCC 307' };
+        this.update1 = this.update1.bind(this);
+        this.update2 = this.update2.bind(this);
+        this.update3 = this.update3.bind(this);
+    }
+    update1(event) {
+        this.setState({ unitecClassName: 'CCC 404' })
+    }
+
+    update2(event) {
+        this.setState({ unitecClassName: 'CCC 594' })
+    }
+
+    update3(event) {
+        this.setState({ unitecClassName: 'LCC 104' })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <label>Nombre de Estudiante: {this.props.unitecStudentName}</label>
+                </div>
+                <div>
+                    <label>Nombre de CLase {this.state.unitecClassName}</label>
+                </div>
+                <button onClick={this.update1} >hola1</button>
+                <button onClick={this.update2} >hola2</button>
+                <button onClick={this.update3} >hola3</button>
+            </div>
+        )
+    }
+}
+
+UnitecClass.propTypes = {
+    unitecStudentName: PropTypes.string.isRequired,
+};
+
+UnitecClass.defaultProps = {
+    unitecStudentName: 'Hello World!',
+};
+
+export default UnitecClass;
